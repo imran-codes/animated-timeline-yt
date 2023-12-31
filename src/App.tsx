79bfc60f9f83dac3d08adef867e005f46c4ffb4b
Imran Hussain <62844767+imran-codes@users.noreply.github.com>
2023-12-31 15:03:59 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import { AppContainer } from "./styles";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <AppContainer>
       <h1>Animated Timeline</h1>
